d80d392968f49d9187a48714ad0db0cd45241d1d
MohammadQutit <m.qutit@student.aaup.edu>
2021-01-05 01:17:44 +0200

    categories final

--- screens/HomeStack/categories.js
+++ screens/HomeStack/categories.js
@@ -10,6 +10,13 @@ import {
 } from 'react-native';
 let {width, height} = Dimensions.get('window');
 export default function catigories({navigation}) {
+
+const GoTOList=(category)=>{
+  navigation.navigate('ListPage');
+  console.log("haha")
+
+}
+
   return (
     <View
       style={{
@@ -27,9 +34,9 @@ export default function catigories({navigation}) {
       <View style={styles.category_container}>
         <TouchableOpacity
           style={styles.category_button}
-          onPress={() => {
-            navigation.navigate('ListPage');
-          }}>
+          onPress={()=>GoTOList('carpenter')
+           
+          }>
           <Image
             source={require('../../assets/carpenter.png')}
             style={styles.category_icon}
@@ -38,14 +45,14 @@ export default function catigories({navigation}) {
           <Text style={styles.texticon}>Carpenter</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.category_button} onPress={() => {}}>
+        <TouchableOpacity style={styles.category_button}  onPress={()=>GoTOList('Plumber')}>
           <Image
             source={require('../../assets/plumber.png')}
-            style={styles.category_icon}
+            style={[styles.category_icon]}
           />
           <Text style={styles.texticon}>Plumber</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.category_button} onPress={() => {}}>
+        <TouchableOpacity style={styles.category_button} onPress={()=>GoTOList('Builder')}>
           <Image
             source={require('../../assets/builder.png')}
             style={styles.category_icon}
@@ -55,7 +62,7 @@ export default function catigories({navigation}) {
       </View>
 
       <View style={styles.category_container}>
-        <TouchableOpacity style={styles.category_button} onPress={() => {}}>
+        <TouchableOpacity style={styles.category_button}  onPress={()=>GoTOList('Electrician')}>
           <Image
             source={require('../../assets/electrician.png')}
             style={styles.category_icon}
@@ -63,44 +70,44 @@ export default function catigories({navigation}) {
           <Text style={styles.texticon}>Electrician</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.category_button} onPress={() => {}}>
+        <TouchableOpacity style={styles.category_button} onPress={()=>GoTOList('Painter')}>
           <Image
-            source={require('../../assets/carpenter.png')}
+            source={require('../../assets/painter.png')}
             style={styles.category_icon}
           />
-          <Text style={styles.texticon}>Carpenter</Text>
+          <Text style={styles.texticon}>Painter</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.category_button} onPress={() => {}}>
+        <TouchableOpacity style={styles.category_button} onPress={()=>GoTOList('Mechanic')}>
           <Image
-            source={require('../../assets/carpenter.png')}
+            source={require('../../assets/mechanic.png')}
             style={styles.category_icon}
           />
-          <Text style={styles.texticon}>Carpenter</Text>
+          <Text style={styles.texticon}>Mechanic</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.category_container}>
-        <TouchableOpacity style={styles.category_button} onPress={() => {}}>
+        <TouchableOpacity style={styles.category_button} onPress={()=>GoTOList('AC Technician')}>
           <Image
-            source={require('../../assets/carpenter.png')}
+            source={require('../../assets/AC.png')}
             style={styles.category_icon}
           />
-          <Text style={styles.texticon}>Carpenter</Text>
+          <Text style={styles.texticon}>AC Technician</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.category_button} onPress={() => {}}>
+        <TouchableOpacity style={styles.category_button} onPress={()=>GoTOList('Aluminum')}>
           <Image
             source={require('../../assets/carpenter.png')}
             style={styles.category_icon}
           />
-          <Text style={styles.texticon}>Carpenter</Text>
+          <Text style={styles.texticon}>Aluminium</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.category_button} onPress={() => {}}>
+        <TouchableOpacity style={styles.category_button} onPress={()=>GoTOList('Blacksmith')}>
           <Image
-            source={require('../../assets/carpenter.png')}
+            source={require('../../assets/blacksmith.png')}
             style={styles.category_icon}
           />
-          <Text style={styles.texticon}>Carpenter</Text>
+          <Text style={styles.texticon}>Blacksmith</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -117,32 +124,39 @@ const styles = StyleSheet.create({
   },
 
   category_button: {
-    height: 100,
-    width: width / 4,
+    height: height/6,
+    width: width/3.5,
     margin: 10,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
+    borderColor:"#F2F2F2",
+    borderWidth:10,
     //shadowRadius:10,
     //shadowColor: 'rgba(0, 0, 0, 0.1)',
   },
 
   category_icon: {
-    height: 100,
-    width: 100,
+    height: (height/6)-5,
+    width: (width/3.5)-3,
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 35,
     borderRadius: 15,
-    backgroundColor:'white',
+    backgroundColor:"white"
+    
   },
 
   texticon: {
+    width:120,
     paddingTop: 15,
     color: 'purple',
     fontWeight: 'bold',
     fontSize: 17,
     fontFamily: 'Cochin',
+    justifyContent:"center",
+    alignItems:"center",
+    textAlign:"center"
 
   },
 });
