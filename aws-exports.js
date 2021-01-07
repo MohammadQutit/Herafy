5cb0d488cf13d22d7b2619737e9fe5d307b5531e
MohammadQutit <m.qutit@student.aaup.edu>
2021-01-07 23:18:04 +0200

    add s3 storage

--- aws-exports.js
+++ aws-exports.js
@@ -7,7 +7,9 @@ const awsmobile = {
     "aws_cognito_region": "eu-west-1",
     "aws_user_pools_id": "eu-west-1_pHga1nCvn",
     "aws_user_pools_web_client_id": "7qi9p52kjml4lpt49jkmh094li",
-    "oauth": {}
+    "oauth": {},
+    "aws_user_files_s3_bucket": "herafy180356-dev",
+    "aws_user_files_s3_bucket_region": "eu-west-1"
 };
 
 
